8339c18c07dc1b3fcab6e3ef6c0ee2f6543a56f7
Roknor <xyzcmp@gmail.com>
2024-02-20 18:08:36 +0200

    made python files executable

--- bin/hyprland-plugin-update.py
+++ bin/hyprland-plugin-update.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
 import subprocess, os
 
 def check_updates():
-    os.system(f'notify-send -h string:x-canonical-private-synchronous:hypr-cfg -u low "Checking for plugin updates"')
+    os.system(f'notify-send -h string:x-canonical-private-synchronous:hypr-cfg -u low "Checking for plugin updates. This might take a while."')
     result = subprocess.run(['hyprpm', 'update'], capture_output=True, text=True)
 
     if 'up-to-date' in result.stdout:
@@ -9,6 +10,7 @@ def check_updates():
         os.system(f'hyprpm disable hyprbars')
     else:
         os.system(f'notify-send -h string:x-canonical-private-synchronous:hypr-cfg -u low "Plugin updates installed successfully"')
+        os.system(f'hyprpm disable hyprbars')
 
 if __name__ == "__main__":
     check_updates()
